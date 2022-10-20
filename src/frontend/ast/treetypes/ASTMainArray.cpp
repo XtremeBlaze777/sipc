60a5cc8adbb61edeef108d98425a44244048717e
XtremeBlaze777 <ratikmathur@gmail.com>
2022-10-20 01:30:34 -0400

    AST node for primary way of instantiating arrays

---
+++ src/frontend/ast/treetypes/ASTMainArray.cpp
@@ -0,0 +1,43 @@
+#include "ASTMainArray.h"
+#include "ASTinternal.h"
+
+ASTMainArray::ASTMainArray(std::vector<std::unique_ptr<ASTExpr>> elements) {
+    for(auto &element : elements) {
+        std::shared_ptr<ASTExpr> e = std::move(element);
+        this->elements.push_back(e);
+    }
+}
+
+std::vector<ASTExpr*> ASTMainArray::getElements() const {
+    return rawRefs(elements);
+}
+
+void ASTMainArray::accept(ASTVisitor* visitor) {
+    if (visitor->visit(this)) {
+        getElements()->accept(visitor);
+        for (auto p : getElements()) {
+        p->accept(visitor);
+        }
+    }
+    visitor->endVisit(this);
+}
+
+std::vector<std::shared_ptr<ASTNode>> ASTFunction::getChildren() {
+    std::vector<std::shared_ptr<ASTNode>> children;
+    for (auto &element : elements) {
+        children.push_back(element);
+    }
+    return children;
+}
+
+std::ostream& ASTArrayMain::print(std::ostream &out) const {
+    auto gotten = getElements();
+    auto &last = gotten.back();
+    gotten.pop_back();
+    out << "[";
+    for (auto &element : gotten) {
+        out << element << ",";
+    }
+    out << last << "]";
+    return out;
+}   // LCOV_EXCL_LINE
---
+++ src/frontend/ast/treetypes/ASTMainArray.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ASTExpr.h"
+#include <ostream>
+
+/*! \brief Class for an array.
+ *
+ */
+class ASTArray: public ASTNode {
+    std::string name;
+    std::vector<std::shared_ptr<ASTExpr>> elements;
+    public:
+        std::vector<std::shared_ptr<ASTNode>> getChildren() override;
+        ASTArray(std::vector<std::unique_ptr<ASTExpr>> elements);
+        std::string getName() const { return name; }
+        std::vector<ASTExpr*> getElements() const;
+        void accept(ASTVisitor * visitor) override;
+        llvm::Value *codegen() override;
+    protected:
+        std::ostream& print(std::ostream &out) const override;
