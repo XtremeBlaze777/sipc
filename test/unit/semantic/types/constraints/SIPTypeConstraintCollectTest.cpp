534df960fbb11dbb1f8726b04bcbf282fd8af31d
Neil Phan <nnp3axx@virginia.edu>
2022-11-04 16:07:15 -0400

    More typeconstraint tests

--- test/unit/semantic/types/constraints/SIPTypeConstraintCollectTest.cpp
+++ test/unit/semantic/types/constraints/SIPTypeConstraintCollectTest.cpp
@@ -200,35 +200,35 @@ TEST_CASE("TypeConstraintVisitor: Binary_UnaryExpr", "[TypeConstraintVisitor]") 
   )";
 
   std::vector<std::string> expected = {
-    "\u27E65@4:11\u27E7 = int", // const int
-    "\u27E62@4:15\u27E7 = int", // const int
-    "\u27E6(5<2)@4:10\u27E7 = bool", // comparison
-    "\u27E6-2@4:22\u27E7 = int", // const int
-    "\u27E67@4:29\u27E7 = int", // const int
-    "\u27E6(-2<=7)@4:21\u27E7 = bool", // comparison
-    "\u27E6(5<2)and(-2<=7)@4:10\u27E7 = bool", // logical and
-    "\u27E6a@4:6\u27E7 = \u27E6(5<2)and(-2<=7)@4:10\u27E7", // assign
-    "\u27E60@5:11\u27E7 = int", // const int
-    "\u27E60@5:16\u27E7 = int", // const int
-    "\u27E6(0>=0)@5:10\u27E7 = bool", // comparison
-    "\u27E6(a)@5:22\u27E7 = \u27E6a@5:6\u27E7", // access
-    "\u27E6(0>=0)or(a)@5:10\u27E7 = bool", // logical or
-    "\u27E6o@5:6\u27E7 = \u27E6(0>=0)or(a)@5:10\u27E7", // assign
-    "\u27E6(o)@5:22\u27E7 = \u27E6o@5:6\u27E7", // access
-    "\u27E6not(o)@6:10\u27E7 = bool", // logical not
-    "\u27E6n@6:10\u27E7 = \u27E6not(o)@6:10\u27E7", // assign
-    "\u27E6-6@7:11\u27E7 = int", // const int
-    "\u27E64@7:15\u27E7 = int", // const int
-    "\u27E6-6%4@7:15\u27E7 = int", // modulus
-    "\u27E6m@7:6\u27E7 = \u27E6-6%4@7:15\u27E7", // assign
-    "\u27E6m@8:6\u27E7 = \u27E6m@7:6\u27E7", // access
-    "\u27E6m++@8:6\u27E7 = int", // inc
-    "\u27E6m@9:6\u27E7 = \u27E6m@7:6\u27E7", // access
-    "\u27E6m--@9:6\u27E7 = int", // dec
-    "\u27E60@10:13\u27E7 = int", // main return int
-    "\u27E60@10:13\u27E7 = int", // int constant
-    "\u27E6main@2:4\u27E7 = () -> \u27E60@9:13\u27E7" // fun declaration
+    "\u27E6((0>=0) or a)@5:10\u27E7 = bool",
+    "\u27E6((5<2) and (-2<=7))@4:10\u27E7 = bool",
+    "\u27E6(-2<=7)@4:23\u27E7 = bool",
+    "\u27E6(-6%4)@7:10\u27E7 = int",
+    "\u27E6(0>=0)@5:11\u27E7 = bool",
+    "\u27E6(5<2)@4:11\u27E7 = bool",
+    "\u27E6-2@4:23\u27E7 = int",
+    "\u27E6-6@7:10\u27E7 = int",
+    "\u27E60@10:13\u27E7 = int",
+    "\u27E60@5:11\u27E7 = int",
+    "\u27E60@5:16\u27E7 = int",
+    "\u27E62@4:15\u27E7 = int",
+    "\u27E64@7:15\u27E7 = int",
+    "\u27E65@4:11\u27E7 = int",
+    "\u27E67@4:29\u27E7 = int",
+    "\u27E6a@3:10\u27E7 = bool",
+    "\u27E6a@3:10\u27E7 = \u27E6((5<2) and (-2<=7))@4:10\u27E7",
+    "\u27E6m++@8:6\u27E7 = int",
+    "\u27E6m--@9:6\u27E7 = int",
+    "\u27E6m@3:17\u27E7 = int",
+    "\u27E6m@3:17\u27E7 = \u27E6(-6%4)@7:10\u27E7",
+    "\u27E6main@2:4\u27E7 = () -> \u27E60@10:13\u27E7",
+    "\u27E6n@3:14\u27E7 = \u27E6not o@6:10\u27E7",
+    "\u27E6not o@6:10\u27E7 = bool",
+
+    "\u27E6o@3:12\u27E7 = bool",
+    "\u27E6o@3:12\u27E7 = \u27E6((0>=0) or a)@5:10\u27E7" 
   };
+  //diff(program, expected);
   runtest(program, expected);
 }
 
@@ -244,25 +244,6 @@ TEST_CASE("TypeConstraintVisitor: ForLoops", "[TypeConstraintVisitor]") {
     }
   )";
 
-  // std::vector<std::string> expected = {
-  //   "\u27E6i@4:25\u27E7 = int", // access
-  //   "\u27E6i@4:11\u27E7 = int", // iterator
-  //   "\u27E65@4:16\u27E7 = int", // const int
-  //   "\u27E6[5]@4:15\u27E7 = arr::\u27E65@4:16\u27E7", // array
-  //   "\u27E6j@5:35\u27E7 = int", // access
-  //   "\u27E6j@5:11\u27E7 = int", // iterator
-  //   "\u27E60@5:15\u27E7 = int", // const int
-  //   "\u27E65@5:20\u27E7 = int", // const int
-  //   "\u27E62@5:25\u27E7 = int", // const int
-  //   "\u27E6k@6:30\u27E7 = int", // access
-  //   "\u27E6k@6:11\u27E7 = int", // iterator
-  //   "\u27E60@6:15\u27E7 = int", // const int
-  //   "\u27E65@6:20\u27E7 = int", // const int
-  //   "\u27E60@7:13\u27E7 = int", // main return int
-  //   "\u27E60@7:13\u27E7 = int", // int constant
-  //   "\u27E6main@2:4\u27E7 = () -> \u27E60@7:13\u27E7" // fun declaration
-  // };
-
   std::vector<std::string> expected = {
       "\u27E60@5:15\u27E7 = int",
       "\u27E60@6:15\u27E7 = int",
@@ -275,7 +256,7 @@ TEST_CASE("TypeConstraintVisitor: ForLoops", "[TypeConstraintVisitor]") {
       "\u27E6[5]@4:15\u27E7 = arr::\u27E65@4:16\u27E7",
       "\u27E6[5]@4:15\u27E7 = arr::\u27E6[5]@4:15\u27E7",
       "\u27E6i@3:10\u27E7 = int",
-      "\u27E6i@3:10\u27E7 =\u03b1<i>",
+      "\u27E6i@3:10\u27E7 = \u03b1<i>",
       "\u27E6j@3:12\u27E7 = int",
       "\u27E6k@3:14\u27E7 = int",
       "\u27E6main@2:4\u27E7 = () -> \u27E60@7:13\u27E7"
